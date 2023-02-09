23fb6185eb6d7ce1c0702f1b03e038825ecc7cdd
Gulnaz Nikitina <110427929+GulnazNikitina@users.noreply.github.com>
2023-02-09 08:25:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 const listItems = []
-
+// some comments
 getData()
 
 filter.addEventListener('input', (e) => filterData(e.target.value))
